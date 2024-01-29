d8b2fcab2cc60d40629ba408bc258889b06eadac
EduardoBrandao <eduardo.loureiro@madeinweb.app>
2024-01-29 19:41:38 -0300

    ajustes migration

--- src/routes/cliente.routes.ts
+++ src/routes/cliente.routes.ts
@@ -5,6 +5,6 @@ const clienteRouter = Router();
 const clienteController = new ClienteController();
 
 clienteRouter.get("/", clienteController.listar);
-//clienteRouter.post("/create", clienteController.create);
+clienteRouter.post("/create", clienteController.create);
 
 export default clienteRouter;
